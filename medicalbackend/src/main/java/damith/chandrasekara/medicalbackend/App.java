3415cc129d07916ca000d1620ef762a7236f2756
damithchandrasekara <damithkum565@gmail.com>
2018-05-13 18:19:07 +0100

    Successfully Added

---
+++ medicalbackend/src/main/java/damith/chandrasekara/medicalbackend/App.java
@@ -0,0 +1,13 @@
+package damith.chandrasekara.medicalbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
